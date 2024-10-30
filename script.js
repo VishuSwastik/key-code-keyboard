5717772cea6e5cf5e68c334b3d5899b06e7f4027
VishuSwastik <vaishnavidadhe10@gmail.com>
2024-10-30 21:15:44 +0530

    key code

---
+++ script.js
@@ -0,0 +1,12 @@
+const displayKey = document.querySelector(".key h2");
+const displayKeyCode = document.querySelector(".keyCode h2");
+const overlay = document.querySelector(".overlay");
+
+window.addEventListener("keydown", (e) => {
+  overlay.classList.add("hide");
+  displayKey.innerHTML = e.key;
+  displayKeyCode.innerHTML = e.keyCode;
+  if (e.keyCode === 32) {
+    displayKey.innerHTML = `'space'`;
+  }
+});
